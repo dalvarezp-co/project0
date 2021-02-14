964e1275c6e46d5176cfa5bcd24fe4ab7dba74d3
Daniela Ricaurte <danielaricaurte@Danielas-MacBook-Air-3.local>
2021-02-14 15:23:38 -0500

    Modified file reader

--- simpleParser.java
+++ simpleParser.java
@@ -1,37 +1,70 @@
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class  SimpleParser
 {
 
-	private boolean answer;
+    private boolean answer;
 
-	private File document;
+    private File document;
 
-	public SimpleParser(String pRuta)
-	{
-		document= new File (pRuta);
-		answer= false;
-	}
+    public SimpleParser(String pRuta)
+    {
+        document = new File (pRuta);
+        answer = true;
+    }
 
-	public boolean documentReader()
-	{
-		FileReader fReader = new FileReader (document);
-		BufferedReader bReader= new BufferedReader(fReader);
+    public boolean documentReader()
+    {
+        FileReader fReader = new FileReader (document);
+        BufferedReader bReader = new BufferedReader(fReader);
+        String line = lector.readLine;
+        int amountOfContinuousEmptyLines = 0;
 
+        while (amountOfcontinuousEmptyLines < 5)
+            if(line == null)
+            {
+                lector.readline;
+                amountOfconutnuousEmptyLines++;
+            }
 
-		String line = lector.readline
+            else
+            {
+                amountOfcontinuousEmptyLines = 0;
 
-		while(line != null)
-		{
+                if (lineSyntaxRevision(line) == false)
+                {
+                    answer = false;
+                    break;
+                }
 
+                lector.readLine;
+            }
+    }
 
-		}
+    fReader.close();
+    bReader.close();
 
-		fReader.close();
-		bReader.close();
+    return answer;
+}
 
-		return answer;
-	}
+public boolean  lineSyntaxRevision( String line)
+{
+    boolean isCorrect = false;
+    line
+
+    if ()
+    {
+
+    }
+
+    return isCorrect;
+}
+
 
 
 }
